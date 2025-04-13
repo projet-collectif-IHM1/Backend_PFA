1fbde57a55de1254536a44bb8d93773d76ecfe76
ramii845 <rami.elfrikha@gmail.com>
2025-04-13 11:36:01 +0100

    first commit2

---
+++ controllers/but_controller.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, HTTPException
+from models.but import But
+from config import SECRET_KEY, MONGO_URI, MONGO_DB
+from passlib.context import CryptContext
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import List
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from bson import ObjectId
+
+
+but_router = APIRouter()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+client = AsyncIOMotorClient(MONGO_URI)
+db = client[MONGO_DB]
+
+@but_router.post("/")
+async def create_paye(paye: But):
+    result = await db.buts.insert_one(paye.dict())
+    return {"id": str(result.inserted_id)} 
+
+@but_router.get("/", response_model=List[But])
+async def get_payes():
+    cats = await db.buts.find().to_list(100)
+
+    # Convertir _id en string et l'ajouter en tant que 'id'
+    for cat in cats:
+        cat["id"] = str(cat["_id"])
+        del cat["_id"]  # Supprimer _id original si nécessaire
+
+    return JSONResponse(status_code=200, content={"status_code": 200, "buts": cats})
+
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware  # 🔴 Ajouté pour le CORS
 from controllers.user_controller import user_router  
 from controllers.categorie_controller import cat_router
 from controllers.background_controller import vid_router
+from controllers.motivation_controller import mot_router
+from controllers.but_controller import but_router
 
 app = FastAPI()
 
@@ -23,6 +25,8 @@ app.add_middleware(
 app.include_router(user_router, prefix="/users", tags=["users"])
 app.include_router(cat_router, prefix="/categories", tags=["categories"])
 app.include_router(vid_router, prefix="/videos", tags=["categories"])
+app.include_router(mot_router, prefix="/motivations", tags=["motivations"])
+app.include_router(but_router, prefix="/buts", tags=["buts"])
 
 @app.get("/")
 async def root():
