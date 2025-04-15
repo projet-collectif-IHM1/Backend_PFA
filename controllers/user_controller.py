4efd9a16894f7bc423eada0741548da605530e46
ramii845 <rami.elfrikha@gmail.com>
2025-04-15 20:20:46 +0100

    first commit3

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -5,11 +5,20 @@ from passlib.context import CryptContext
 from motor.motor_asyncio import AsyncIOMotorClient
 import jwt
 import datetime
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from bson import ObjectId
+from typing import List, Optional
 
 user_router = APIRouter()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[MONGO_DB]
+def get_objectid(id: str):
+    try:
+        return ObjectId(id)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid ID format")
 
 @user_router.post("/register/")
 async def register(user: User):
@@ -42,3 +51,38 @@ async def signin(user_data: dict):
     )
 
     return {"token": token, "role": existing_user.get("role", "user"), "message": "Connexion réussie"}
+@user_router.get("/", response_model=List[User])
+async def get_users():
+    users = await db.users.find().to_list(100)
+
+    # Convertir _id en string et l'ajouter en tant que 'id'
+    for user in users:
+        user["id"] = str(user["_id"])
+        del user["_id"]  # Supprimer _id original si nécessaire
+
+    return JSONResponse(status_code=200, content={"status_code": 200, "users": users})
+
+@user_router.put("/{user_id}", response_model=dict)
+async def update_user(user_id: str, user: User):
+    result = await db.users.update_one({"_id": get_objectid(user_id)}, {"$set": user.dict()})
+    if result.modified_count == 0:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"message": "User updated successfully"}
+
+@user_router.delete("/{user_id}", response_model=dict)
+async def delete_user(user_id: str):
+    # Supprimer d'abord toutes les réservations associées à cet utilisateur
+
+    result = await db.users.delete_one({"_id": ObjectId(user_id)})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"message": "User deleted successfully"}
+
+
+@user_router.get("/{user_id}", response_model=User)
+async def get_user_by_id(user_id: str):
+    user = await db.users.find_one({"_id": ObjectId(user_id)})
+    if not user:
+        raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
+    
+    return user
